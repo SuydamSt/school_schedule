38491d7021bcc57b21b70c054b549d270ecd4cf8
SuydamSt <clarkperritt@icloud.com>
2026-01-01 15:57:10 -0500

    v0.1.0

---
+++ UPDATES.md
@@ -0,0 +1,5 @@
+v0.1.0
+GOAL: create and run a minimal streamlit page
+
+- app.py created
+- no files changed
---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="v0.1", layout="wide")
+st.title("school schedule v0.1")
+st.write("testing_text")
